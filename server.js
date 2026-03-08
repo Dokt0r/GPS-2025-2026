86cc80adcc8296ab7a3b62e3d2dc7e54bed1e308
Dokt0r <ssante@ucm.es>
2026-03-08 19:58:39 +0100

    Add initial HTML structure and CSS styles for the SmartFridge project

---
+++ server.js
@@ -0,0 +1,15 @@
+// 1. Importamos la lógica de la aplicación desde src/app.js
+const app = require('./src/app');
+
+// 2. Importamos dotenv para leer las variables del archivo .env
+require('dotenv').config();
+
+// 3. Definimos el puerto
+// Usará el del archivo .env o el 3000 por defecto si aquel no existe
+const PORT = process.env.PORT || 3000;
+
+// 4. Arrancamos el servidor
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor de la Nevera Virtual funcionando en: http://localhost:${PORT}`);
+    console.log('Presiona CTRL+C para detener el servidor');
+});
